ac51fc47164b2913359763ac94fece41a18b234d
Harry Wickham <harrywickham@live.co.uk>
2022-11-07 17:54:12 +0000

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { getLayout } from "../layouts/MapLayout";
 
 export default function IndexPage() {
-  return <h1>Index page</h1>;
+  return <h1>Index page2</h1>;
 }
 
 IndexPage.getLayout = getLayout;
