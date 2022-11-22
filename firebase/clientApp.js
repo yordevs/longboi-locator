e9b2e8a09783963b809287938d8f7af7e944e1c9
George Howarth <george@pyesmeadow.com>
2022-11-22 13:22:02 +0000

    Call firebase init code

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -20,10 +20,7 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth();
-export const provider = new GoogleAuthProvider();
-export const storage = getStorage(app);
-provider.setCustomParameters({ prompt: "select_account" });
+export const initFirebase = () => {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
+import { useEffect } from "react";
+import { initFirebase } from "../firebase/clientApp.js";
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    if (window) {
+      initFirebase();
+    }
+  }, []);
+
+
   return <Component {...pageProps} />
 }
 
